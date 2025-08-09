46d18621b43dbeca875820db610e5b42a72652fd
almaas21 <44881468+almaas21@users.noreply.github.com>
2025-08-09 12:40:52 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,5 +23,5 @@ async def remove_background(file: UploadFile = File(...)):
     return {"image_bytes": list(output_image)}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
